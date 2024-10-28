d74f1f8f01364af57eca976e80ec3aec3563b3f0
Wlamir Alves <99266950+wlamiralves@users.noreply.github.com>
2024-10-28 09:23:17 -0300

    Update initialize_db.py

--- taskmanager/initialize_db.py
+++ taskmanager/initialize_db.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 def initialize_db():
-    # Conectar ao banco de dados SQLite (ou criar o banco se não existir)
+   
     conn = sqlite3.connect('task_reports.db')
     cursor = conn.cursor()
     
-    # Criar a tabela para armazenar informações sobre os relatórios
+    
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS reports (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,6 +16,6 @@ def initialize_db():
     conn.commit()
     conn.close()
 
-# Chame a função para inicializar o banco de dados
+
 if __name__ == '__main__':
     initialize_db()
